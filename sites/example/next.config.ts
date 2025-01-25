e3d693ac48a6c087a0044304913eabba6f992672
YasinChan <yasinchan2016@gmail.com>
2025-01-25 16:12:19 +0800

    feat: update next config

--- sites/example/next.config.ts
+++ sites/example/next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
   output: "export",
-  images: {
-    unoptimized: true,
-  },
   basePath: '/slate-canvas',
-  assetPrefix: '/slate-canvas/',
 };
 
 export default nextConfig;
